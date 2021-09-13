7f946de9c484319c9912f5e23fd7cb3da30b676d
Umangi Prajapati <umangip96@gmail.com>
2021-09-13 16:47:26 -0400

    all scripts update commit

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,5 +1,10 @@
-var speakWord = "Hello";
+(function(window) {
+  
+  var helloSpeaker = {}
+  var speakWord = "Hello";
 
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.helloSpeaker = helloSpeaker;
+})(window);
